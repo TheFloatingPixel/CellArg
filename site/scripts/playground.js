90161cd237aa3a386c8baa22f92f49208ab2b611
TheFloatingPixel <110129491+TheFloatingPixel@users.noreply.github.com>
2023-02-22 16:46:19 +0100

    Implement playground editor

---
+++ site/scripts/playground.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', () => {
+	
+	document.querySelector('.run').addEventListener('click', () => {
+		
+		const code = document.querySelector(`.editor`).value;
+		let cellArg = new CellArg(code);
+		
+		cellArg.run();
+		
+		document.querySelector(`.output`).innerText = cellArg.output;
+		
+	});
+	
+	//document.querySelector('.debug').addEventListener('click', () => {
+	//	alert(`Not imeplemented.`);
+	//});
+	
+});
